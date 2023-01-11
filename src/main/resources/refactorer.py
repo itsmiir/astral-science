2cc96ad3ffeefdffae6e5e559986ed0c255da7ba
Felix <68397506+feluxerich@users.noreply.github.com>
2023-01-11 12:11:02 +0100

    Refactoring and refactoring refactorer.py

--- src/main/resources/refactorer.py
+++ src/main/resources/refactorer.py
@@ -6,32 +6,19 @@ directory = 'data/astralscience'
 # iterate over files in
 # that directory
 def check(f):
-        # checking if it is a file
-        if os.path.isdir(f):
-            for filename2 in os.listdir(f):
-                f2 = os.path.join(f, filename2)
-                if os.path.isdir(f2):
-                    check(f2)
-                if os.path.isfile(f2):
-                    with open(f2, "r") as file:
-                        try:
-                            filedata = file.read()
-
-                            # Replace the target string
-                            filedata = filedata.replace('astral_science', 'astralscience')
-                            filedata = filedata.replace('ntrstlr', 'astralscience')
-                            with open(f2, 'w') as file:
-                                file.write(filedata)
-                            with open(f2, "r") as file:
-                                for line in file:
-                                    print(line)
-
-                        except UnicodeDecodeError as e:
-                            pass
-                        
-
-                        # Write the file out again
-                    
+    if not os.path.isdir(f):
+        return
+    for filename2 in os.listdir(f):
+        f2 = os.path.join(f, filename2)
+        if os.path.isdir(f2):
+            return check(2)
+        with open(f2, 'w+') as file:
+            try:
+                filedata = file.read().replace('astral_science', 'astralscience').replace('ntrstlr', 'astralscience')
+                file.write(filedata)
+                [print line for line in file.readlines()]
+            except UnicodeDecodeError as _:
+                pass
 
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
